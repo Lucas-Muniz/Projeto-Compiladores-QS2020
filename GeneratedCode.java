3497ac3023dab984bf1afdf91a7bcef276537bd1
danieldigenova <58534252+danieldigenova@users.noreply.github.com>
2020-12-14 08:22:50 -0300

    Programa exemplo

--- GeneratedCode.java
+++ GeneratedCode.java
@@ -14,13 +14,19 @@ public class GeneratedCode{
         double  i;
         boolean  bol;
 
+        /* Comandos de leitura 
+                       e escrita */
         System.out.println("Programa Teste");
         System.out.println("Digite A");
         a = _key.nextDouble();
         System.out.println("Digite B");
         b = _key.nextDouble();
+        System.out.println("Digite bol");
         bol = _key.nextBoolean();
+        /* Declaração de uma nova variável com valor atribuído */
         bool = true ;
+        frase = "Hello world";
+        /* Comando se - senao */
         if (a > b) {
             c = a + b;
         } else {
@@ -30,52 +36,48 @@ public class GeneratedCode{
         if (a != b) {
             c = a + b;
         }
-        t = _key.nextLine();
-        t = "hello" + " " + ("world");
-        t = "Linguagem " + t;
-        f = (a + c) / (2 + 5.6);
-        f = (-10.525) / ((+10));
-        f = 10 + ((-10)) + (-1);
-        f = (-10.05) - (+10);
-        f = a + b;
-        frase = "ello world";
-        f = a - 215.454;
-        bol = f < 10 || (true && false) || !(false) && f <= 10 / 2;
-        bol = a + 1 < b && f <= 10 / 2;
         if (bol ) {
             t = "e verdadeiro";
         }
-        /* Condicao */
         if (a == b) {
             t = "igual";
         } else {
             t = "diferente";
         }
 
+        /* Concatenação de Strings */
+        t = "hello" + " " + ("world");
+        t = "Linguagem " + t;
+        /* Operações numéricas */
+        f = (a + c) / (2 + 5.6);
+        f = (-10.525) / ((+10));
+        f = 10 + ((-10)) + (-1);
+        f = (-10.05) - (+10);
+        f = a + b;
+        f = a - 215.454;
+        /* Operações booleanas */
+        bol = f < 10 || (true && false) || !(false) && f <= 10 / 2;
+        bol = a + 1 < b && f <= 10 / 2;
+        /* Comando faca ... enquanto */
         do {
             a = _key.nextDouble();
             System.out.println(a);
         } while (a != 0 || ( f == 0) && false);
 
-        System.out.println("C e igual a ");
-        System.out.println(c);
-        d = c * (a + b);
-        System.out.println("D e igual a ");
-        System.out.println(d);
+        /* Comando enquanto */
         while (a + 1 < b && true) {
             a = _key.nextDouble();
             System.out.println(a);
             System.out.println("TESTE 1");
             a = a + 1;
         }
-        /* comentario de 
-          		varias linhas  */
-        /**/
+        /* Comando para  */
         for (i=0.2; i < 2 * (5 + f); i=i+1) {
             System.out.println(a);
             System.out.println(i);
             a = a + 2;
         }
+        /* Comando escolha */
         switch (t) {
             case "aa" : 
                 System.out.println("aa");
